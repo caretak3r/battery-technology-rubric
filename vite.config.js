43f3d8c525861ea9dba84f5b23c9a93ef418fc26
rohit <50377477+caretak3r@users.noreply.github.com>
2025-07-22 08:09:24 -0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    // Modify base path for GitHub Pages deployment
+    config.base = '/YOUR_REPOSITORY_NAME/'
+  }
+
+  return config
+})
